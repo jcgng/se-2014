77120b03f8b12a580930e23df37266507ef507fc
jcror@iscte-iul.pt <jcror@iscte-iul.pt@355ed251-9562-62f1-27d7-a349075709df>
2014-06-09 01:42:58 +0000

    BPM sketch.

---
+++ trunk/BPM.ino
@@ -0,0 +1,15 @@
+void saveBPM() {
+    if(lastBPM[9]==0) { // not filled
+      for(int i=0;i<10;i++) {
+        if(lastBPM[i]==0) {
+          lastBPM[i] = BPM; break;
+        }
+      }
+    } else { // filled
+      for(int i=0;i<=8;i++) {
+        lastBPM[i] = lastBPM[i+1];
+      }
+      lastBPM[9] = BPM;
+    }
+}
+
